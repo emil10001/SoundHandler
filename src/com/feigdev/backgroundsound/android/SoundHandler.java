9663a48ef05fbf54a45c30190b9cddc9aa2b65e8
E. John Feig <e.john.feig@gmail.com>
2012-01-21 14:43:32 -0500

    the basics have been implemented

--- src/com/feigdev/backgroundsound/android/SoundHandler.java
+++ src/com/feigdev/backgroundsound/android/SoundHandler.java
@@ -8,16 +8,16 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 
 public class SoundHandler {
-	MediaPlayer music;
-//	MediaPlayer sfx1;
-//	MediaPlayer sfx2;
-//	MediaPlayer sfx3;
+	private MediaPlayer music;
+	private MediaPlayer sfx1 = null;
+	private MediaPlayer sfx2 = null;
+	private MediaPlayer sfx3 = null;
+	public static final int SFX1 = 101011;
+	public static final int SFX2 = 101012;
+	public static final int SFX3 = 101013;
 	
 	public SoundHandler(){
 		music = new MediaPlayer();
-//		sfx1 = new MediaPlayer();
-//		sfx2 = new MediaPlayer();
-//		sfx3 = new MediaPlayer();
 	}
 	
 	public void netInit(String path){
@@ -61,12 +61,88 @@ public class SoundHandler {
 			e.printStackTrace();
 		}
 	}
-	
-	public void stopMusic(){
-		music.stop();
+
+	public void initSfx(Context c, int resource, int sfxNum){
+		try {
+			switch (sfxNum){
+			case SFX1:
+				if (sfx1 != null){
+					return;
+				}
+				sfx1 = MediaPlayer.create(c,resource);
+				sfx1.setLooping(false);
+				break;
+			case SFX2:
+				if (sfx2 != null){
+					return;
+				}
+				sfx2 = MediaPlayer.create(c,resource);
+				sfx2.setLooping(false);
+				break;
+			case SFX3:
+				if (sfx3 != null){
+					return;
+				}
+				sfx3 = MediaPlayer.create(c,resource);
+				sfx3.setLooping(false);
+				break;
+			}
+			
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 	}
 	
+	public void localInitSfx(Context c, Uri file, int sfxNum){
+		try {
+			switch (sfxNum){
+			case SFX1:
+				sfx1.reset();
+				sfx1.setAudioStreamType(AudioManager.STREAM_MUSIC);
+				sfx1.setDataSource(c,file);
+				sfx1.setLooping(false);
+				break;
+			case SFX2:
+				sfx2.reset();
+				sfx2.setAudioStreamType(AudioManager.STREAM_MUSIC);
+				sfx2.setDataSource(c,file);
+				sfx2.setLooping(false);
+				break;
+			case SFX3:
+				sfx3.reset();
+				sfx3.setAudioStreamType(AudioManager.STREAM_MUSIC);
+				sfx3.setDataSource(c,file);
+				sfx3.setLooping(false);
+				break;
+			}
+			
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+
 	public void startMusic(){
+		if (music.isPlaying()){
+			return;
+		}
 		try {
 			music.prepare();
 		} catch (IllegalStateException e) {
@@ -79,10 +155,45 @@ public class SoundHandler {
 		music.start();
 	}
 	
+	public void playSfx(int sfxNum){
+		try {
+			switch (sfxNum){
+			case SFX1:
+				if (sfx1.isPlaying()){
+					return;
+				}
+				sfx1.start();
+				break;
+			case SFX2:
+				if (sfx2.isPlaying()){
+					return;
+				}
+				sfx2.start();
+				break;
+			case SFX3:
+				if (sfx3.isPlaying()){
+					return;
+				}
+				sfx3.start();
+				break;
+			}
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+
+	public void stopMusic(){
+		if (!music.isPlaying()){
+			return;
+		}
+		music.stop();
+	}
+	
 	public void killPlayers(){
 		music.release();
-//		sfx1.release();
-//		sfx2.release();
-//		sfx3.release();
+		sfx1.release();
+		sfx2.release();
+		sfx3.release();
 	}
 }
